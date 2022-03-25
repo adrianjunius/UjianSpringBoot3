a97a772f825edbdac9c1e1343483d782f3b5a082
adrianjunius <benefitadrian@gmail.com>
2022-03-26 01:55:41 +0700

    post pertama

---
+++ src/main/java/com/springboot/controller/WebController.java
@@ -0,0 +1,39 @@
+package com.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.springboot.model.TitleModel;
+
+@Controller
+public class WebController {
+	
+	TitleModel title = new TitleModel("BEKEN","Trans");
+	
+	@GetMapping("/")
+	private String index(Model model) {
+		model.addAttribute("titleModel", title);
+		return "index";
+	}
+	@GetMapping("/about")
+	private String about(Model model) {
+		model.addAttribute("titleModel", title);
+		return "about";
+	}
+	@GetMapping("/service")
+	private String service(Model model) {
+		model.addAttribute("titleModel", title);
+		return "service";
+	}
+	@GetMapping("/project")
+	private String project(Model model) {
+		model.addAttribute("titleModel", title);
+		return "project";
+	}
+	@GetMapping("/contact")
+	private String contact(Model model) {
+		model.addAttribute("titleModel", title);
+		return "contact";
+	}
+}
---
+++ src/main/java/com/springboot/model/TitleModel.java
@@ -0,0 +1,13 @@
+package com.springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TitleModel {
+	private String title1;
+	private String title2;
+}
