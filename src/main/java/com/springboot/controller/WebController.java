c1e0e900b622b194c24ea9cc9a1f86d51de4ebd6
Dini Hanifia <dinihanifiaa@gmail.com>
2022-03-27 19:24:54 +0700

    update an dini

--- src/main/java/com/springboot/controller/WebController.java
+++ src/main/java/com/springboot/controller/WebController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.springboot.model.PenumpangModel;
@@ -62,31 +63,68 @@ public class WebController {
 		return "penumpang";
 	}
 	
-	@GetMapping("/penumpang/detailpenumpang")
-	private String detailpenumpang(Model model) {
-		model.addAttribute("titleModel", title);
-		model.addAttribute("listPenumpang", penumpangRepo.findAll());
-		return "detailpenumpang";
+	@GetMapping("/penumpang/formpenumpangbaru")
+	private String penumpangDaftar(Model model) {
+		model.addAttribute("penumpangModel", new PenumpangModel());
+		return "formpenumpangbaru";
 	}
 	
-	@GetMapping("/penumpang/formpenumpangbaru")
+//	@PostMapping("/penumpang/formpenumpangbaru/detailpenumpang")
+//	private String savePenumpang(@ModelAttribute PenumpangModel data) {
+//		penumpangRepo.save(data);
+//		return "detailpenumpang";
+//	}
+	
+//	@GetMapping("/penumpang/detailpenumpang")
+//	private String detailpenumpang(Model model) {
+//		model.addAttribute("titleModel", title);
+//		model.addAttribute("listPenumpang", penumpangRepo.findAll());
+//		return "detailpenumpang";
+//	}
+	
+	@GetMapping("/register")
 	private String formpenumpangbaru(Model model) {
 		model.addAttribute("titleModel", title);
 		model.addAttribute("PenumpangModel", new PenumpangModel());
 		return "formpenumpangbaru";
 	}
-
-	@PostMapping("penumpang/formpenumpangbaru")
-	private String saveDetailPenumpang(@ModelAttribute PenumpangModel data) {
-		penumpangRepo.save(data);
+	
+	@GetMapping("/formpenumpangbaru/detailpenumpang")
+	private String detailPenumpang(Model model) {
+		model.addAttribute("penumpangModel", new PenumpangModel());
 		return "redirect:/detailpenumpang";
 	}
-	@GetMapping("/detailpenumpang")
-	private String detailpenumpangshow(Model model) {
-		model.addAttribute("titleModel", title);
-		model.addAttribute("listPenumpang", penumpangRepo.findAll());
-		return "detailpenumpang";
+	
+	@PostMapping("/formpenumpangbaru/detailpenumpang")
+	private String tambahPenumpang(@ModelAttribute PenumpangModel penumpang) {
+		penumpangRepo.save(penumpang);
+		return "redirect:/detailpenumpang";
 	}
+	
+	@GetMapping("/formlogin")
+	private String masuk(Model model) {
+		model.addAttribute("PenumpangModel", new PenumpangModel());
+		return "formlogin";
+	}
+	
+	@GetMapping("/formlogin/detailpenumpang")
+	private String loginNik2(@PathVariable(value="nik")String nik) {
+		if(penumpangRepo.findByNik(nik).isEmpty()) {
+			return "redirect:/kenihilan";
+		}
+		return "redirect:/detailpenumpang";
+		
+	}
+	
+//	@PostMapping("/formlogin")
+//	private S
+	
+	@GetMapping("/detailpenumpang")
+	private String detailLogin(@PathVariable(value="nik")String nik) {
+		penumpangRepo.findByNik(nik);
+		return "redirect:/detailpenumpang";
+	}
+	
 	//@GetMapping("/cariKeberangkatan")
 //	private List<BookingCustomeGetNameModel> getAllDataTerminalAwalTanggal(
 //			@RequestParam(name = "terminal") String terminal, @RequestParam(name = "tanggal") String tanggal) {
--- src/main/java/com/springboot/repository/PenumpangRepository.java
+++ src/main/java/com/springboot/repository/PenumpangRepository.java
@@ -1,9 +1,14 @@
 package com.springboot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.model.PenumpangModel;
 
 public interface PenumpangRepository extends JpaRepository<PenumpangModel, String>{
+	
+	List<PenumpangModel> getByNik(String nik);
+	List<PenumpangModel> findByNik(String nik);
 
 }
